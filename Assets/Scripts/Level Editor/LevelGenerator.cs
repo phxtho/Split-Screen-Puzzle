77f52b2e2909d02984b1131a53ccfe3f9fb62891
phxtho <37331563+phxtho@users.noreply.github.com>
2018-05-23 07:29:24 +0200

    Basic Level Editor Dun

---
+++ Assets/Scripts/Level Editor/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+
+    public ColourToPrefab[] colourMappings;
+
+    private void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        for(int x = 0; x < map.width; x++)
+        {
+            for(int z = 0; z< map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+    }
+
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColour = map.GetPixel(x, z);
+
+        //Skips transparent pixels
+        if (pixelColour.a == 0)
+            return;
+
+        foreach(ColourToPrefab colourMapping in colourMappings)
+        {
+            if (colourMapping.colour.Equals(pixelColour))
+            {
+                Vector3 position = new Vector3(x, 0, z);
+                Instantiate(colourMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
